31df6591b539d61a70c2da0e73937b5d36526a88
aminebouaziz <bouaziz.amine.18@gmail.com>
2018-12-25 17:27:37 +0100

    third commit

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use App\Entity\Article;
 
 class BlogController extends AbstractController
@@ -30,15 +36,56 @@ class BlogController extends AbstractController
             'age'=>14
         ]);
     }
+
+    /**
+     * @Route("/blog/new",name="blog_create")
+     * @Route("/blog/{id}/edit",name="blog_edit")
+     */
+    public function form(Article $article = null,Request $request,ObjectManager $manager){
+        if(!$article){
+            $article = new Article();
+
+        }
+
+       /* $form= $this->createFormBuilder($article)
+                    ->add('title')
+                    ->add('content')
+                    ->add('image')
+                    ->getForm();
+       */
+
+
+        $form= $this->createForm(ArticleType::class,$article); // avec cls
+
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            if(!$article->getId()){
+                $article->setCreatedAt(new \DateTime());
+            }
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show',[
+                'id'=>$article->getId()
+            ]);
+        }
+        return   $this->render('blog/create.html.twig',[
+          'formArticle'=> $form->createView(),
+            'editMode'=> $article->getId() !== null
+         ]);
+    }
+
      /**
      * @Route("/blog/{id}",name="blog_show")
      */
     public function show(Article $article){
 
 
-       
+
         return $this->render('blog/show.html.twig',[
             'article'=> $article
         ]);
     }
+
 }
